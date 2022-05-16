27f80656ff1d4a4708d59df57e2ef5a9706d0814
Karlis Berzins <berzinsh.karlis@gmail.com>
2022-05-16 16:30:09 +0300

    Create placeholder Dashboard screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './screens/dashboard.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -17,7 +19,7 @@ class MyApp extends StatelessWidget {
       ),
       home: const Scaffold(
         body: SafeArea(
-          child: Text('Mobile Bank App'),
+          child: Dashboard(),
         ),
       ),
     );
