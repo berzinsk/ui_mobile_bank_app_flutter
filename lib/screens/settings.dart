ffb22b3e3ac092742c4a4dbab132cfa9c03a0a1b
Karlis Berzins <berzinsh.karlis@gmail.com>
2022-06-27 15:58:08 +0300

    WIP: add footer button

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -14,9 +14,34 @@ class Settings extends StatelessWidget {
         bottom: false,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: const [
-            ProfileHeader(),
-            Menu(),
+          children: [
+            const ProfileHeader(),
+            const Menu(),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              color: const Color(0xfff5f5f5),
+              child: TextButton(
+                child: const Text(
+                  'Log out',
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w600,
+                    color: Color(0xffee1111),
+                  ),
+                ),
+                onPressed: () {},
+                style: ButtonStyle(
+                  fixedSize:
+                      MaterialStateProperty.all(const Size.fromHeight(40)),
+                  shape: MaterialStateProperty.all(
+                    RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5),
+                        side: const BorderSide(color: Color(0xffee1111))),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
